3d507c294513771db2f0d04fb9fc432ba4a36048
samirsukla <sk.sukla@outlook.com>
2020-04-05 20:46:17 +0530

    Published the Document and verified the updated blog

--- src/main/java/com/BloomReach/Pages/ChannelPage.java
+++ src/main/java/com/BloomReach/Pages/ChannelPage.java
@@ -1,9 +1,35 @@
 package com.BloomReach.Pages;
 
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class ChannelPage {
-  @Test
-  public void f() {
-  }
+import com.BloomReach.config.PropertiesFile;
+
+public class ChannelPage extends ContentPage {
+	public Properties props;
+	public WebDriver driver;
+	WebDriverWait wait;
+
+	public ChannelPage(WebDriver driver) throws Exception {
+		super(driver);
+		props = PropertiesFile.prop;
+		int time = 120;
+		wait = new WebDriverWait(driver, time);
+		PropertiesFile.readContentPropertiesFile();
+		PropertiesFile.readChannelPropertiesFile();
+	}
+	
+	//Check If the Changes are reflected in the blog
+	public boolean checkTheUpdatedSummary() {
+		String fullText = super.getTextFromLabel(props.getProperty("summarydetails"));
+		if(fullText.contains(props.getProperty("editText"))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
--- src/test/java/com/BloomReach/Pages/ChannelPageTest.java
+++ src/test/java/com/BloomReach/Pages/ChannelPageTest.java
@@ -1,9 +1,33 @@
 package com.BloomReach.Pages;
 
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-public class ChannelPageTest {
-  @Test
-  public void f() {
-  }
+import com.BloomReach.Utility.BrowserSetup;
+import com.BloomReach.config.PropertiesFile;
+
+public class ChannelPageTest extends BrowserSetup {
+	public WebDriver driver;
+    public static Properties props;
+    public ChannelPage channel;
+    
+    @BeforeSuite
+    public void setUpChannelPage() throws Exception{
+        driver = getDriver();
+        channel = new ChannelPage(driver);
+        props = PropertiesFile.prop;
+    }
+    
+    //verify the updated content in the channel page
+    @Test(priority = 101, enabled = true)
+   	public void verifyClickingUKChannel()
+   	{
+       	boolean flag = channel.checkTheUpdatedSummary();
+      	Assert.assertTrue(flag, "Summary is not displaying with updated Content");
+   		
+   	}
 }
